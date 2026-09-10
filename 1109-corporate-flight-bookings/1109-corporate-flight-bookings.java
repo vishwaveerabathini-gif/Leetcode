e3bb36fb0ecb30922a4ebf16050b3d468754f743
Vishwa Teja <vishwaveerabathini@gmail.com>
2026-09-10 14:33:15 +0530

    Time: 1305 ms (5.06%), Space: 84.7 MB (48.42%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans=new int[n];
+        for(int i=0;i<bookings.length;i++){
+            for(int j=bookings[i][0]-1;j<bookings[i][1];j++){
+                ans[j]+=bookings[i][2];
+            }
+        }
+        return ans;
+    }
+}
