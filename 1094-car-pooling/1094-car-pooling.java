4055ebf0e8e4ee5fea5e1620291b16d0ca3f770d
Vishwa Teja <vishwaveerabathini@gmail.com>
2026-09-12 11:23:57 +0530

    Time: 40 ms (5.13%), Space: 46.9 MB (11.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(trips,(a,b)->Integer.compare(a[1],b[1]));
         HashMap<Integer,Integer> map=new HashMap<>();
         int count=trips[0][0];
-        ArrayList<Integer> arr=new ArrayList<>();
+        // ArrayList<Integer> arr=new ArrayList<>();
         map.put(trips[0][2],trips[0][0]);
         for(int i=1;i<trips.length;i++){
             if(count>capacity){
@@ -12,15 +12,15 @@ class Solution {
             for(int x:map.keySet()){
                 if(trips[i][1]>=x){
                     count-=map.get(x);
-                    arr.add(x);
+                    map.put(x,0);
                 }
             }
-            for(int x:arr){
-                if(map.containsKey(x)){
-                    map.remove(x);
-                }
+            // for(int x:arr){
+            //     if(map.containsKey(x)){
+            //         map.remove(x);
+            //     }
                 
-            }
+            // }
             if(map.containsKey(trips[i][2])){
                 map.put(trips[i][2],map.get(trips[i][2])+trips[i][0]);
             }else{
